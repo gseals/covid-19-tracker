d64e32dfe8d8a0a4bab898272ced77b0afcc9800
Gabriel Seals <sealsgj@gmail.com>
2020-06-18 11:41:26 -0500

    Updates

---
+++ precache-manifest.cb63f2da199f80a2545d7dd280e6422c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4eb56536e3dda3425ecf80cb9b545a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "5bc0bcafeffc13e279a7",
+    "url": "/covid-19-tracker/static/css/main.7200f731.chunk.css"
+  },
+  {
+    "revision": "2b2b7b5e30829452dd74",
+    "url": "/covid-19-tracker/static/js/2.d7734056.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.d7734056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc0bcafeffc13e279a7",
+    "url": "/covid-19-tracker/static/js/main.824dd49a.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/covidIMG.d7265326.png"
+  }
+]);
